1bb53daeaa8b024a506cffbefe9835e0f67412ca
mfl4 <mhdfarhanl6267@gmail.com>
2024-06-26 12:15:15 +0700

    feat(resources): make view for home page

---
+++ resources/views/home.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>{{ $title }}</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+    <table>
+        <thead>
+            <th><a href="/" class="active">Home</a></th>
+            <th><a href="/about">About</a></th>
+            <th><a href="/blog">Blog</a></th>
+            <th><a href="/contact">Contact</a></th>
+        </thead>
+    </table>
+    <center>
+        <h1>{{ $title }} Page</h1>
+    </center>
+    <script src="js/script.js"></script>
+</body>
+
+</html>
