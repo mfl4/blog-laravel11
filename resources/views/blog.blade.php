58717c4c6e82393b55f19a58bc3cad54205b80de
mfl4 <mhdfarhanl6267@gmail.com>
2024-06-26 12:16:36 +0700

    feat(resources): make view for blog page

---
+++ resources/views/blog.blade.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>{{ $title }}</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+    <table>
+        <thead>
+            <th><a href="/">Home</a></th>
+            <th><a href="/about">About</a></th>
+            <th><a href="/blog" class="active">Blog</a></th>
+            <th><a href="/contact">Contact</a></th>
+        </thead>
+    </table>
+    <center>
+        <h1>{{ $title }} Page</h1>
+
+        <article>
+            <h3>Post Title 1</h3>
+            <h5>Posted by Me</h5>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Soluta ea quam nihil quibusdam aliquid officia
+                deleniti enim animi provident, deserunt, est cupiditate nostrum veritatis odit distinctio earum
+                blanditiis accusantium explicabo dolorum placeat. Nemo quasi rerum sapiente. Numquam sapiente recusandae
+                dolor. Dolor, libero eligendi sunt quaerat atque autem consequuntur minima molestiae nisi ullam. Quaerat
+                repudiandae, voluptatem mollitia in consequuntur doloremque accusantium quasi eum. Non vitae fugiat,
+                culpa similique aliquam quidem libero recusandae magni et corporis tenetur nulla voluptatum ex
+                perspiciatis! Non tempore nemo provident porro, ab aperiam animi dolor ipsum, quasi ea explicabo amet
+                accusamus corporis quaerat quo praesentium harum eveniet!</p>
+        </article>
+        <article>
+            <h3>Post Title 2</h3>
+            <h5>Posted by Yous</h5>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Soluta ea quam nihil quibusdam aliquid officia
+                deleniti enim animi provident, deserunt, est cupiditate nostrum veritatis odit distinctio earum
+                blanditiis accusantium explicabo dolorum placeat. Nemo quasi rerum sapiente. Numquam sapiente recusandae
+                dolor. Dolor, libero eligendi sunt quaerat atque autem consequuntur minima molestiae nisi ullam. Quaerat
+                repudiandae, voluptatem mollitia in consequuntur doloremque accusantium quasi eum. Non vitae fugiat,
+                culpa similique aliquam quidem libero recusandae magni et corporis tenetur nulla voluptatum ex
+                perspiciatis! Non tempore nemo provident porro, ab aperiam animi dolor ipsum, quasi ea explicabo amet
+                accusamus corporis quaerat quo praesentium harum eveniet!</p>
+        </article>
+    </center>
+</body>
+
+</html>
