2081f01d2b511d8ac7a38443bda62f047a7d1160
mfl4 <mhdfarhanl6267@gmail.com>
2024-06-30 06:25:51 +0700

    refac(resources,app): make more modular, reusable & compact layouts by using blade components

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,31 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>{{ $title }}</title>
-    <link rel="stylesheet" href="css/style.css">
-</head>
-
-<body>
-    <table>
-        <thead>
-            <th><a href="/">Home</a></th>
-            <th><a href="/about">About</a></th>
-            <th><a href="/blog">Blog</a></th>
-            <th><a href="/contact" class="active">Contact</a></th>
-        </thead>
-    </table>
-    <center>
-        <h1>{{ $title }} Page</h1>
-        <ul>
-            <li><a href="https://www.instagram.com/mfl__4/" target="_blank">Instagram</a></li>
-            <li><a href="https://www.linkedin.com/in/mfl4/" target="_blank">LinkedIn</a></li>
-            <li><a href="https://github.com/mfl4/" target="_blank">Github</a></li>
-        </ul>
-    </center>
-</body>
-
-</html>
+<x-layout>
+    <x-slot:title>{{ $title }}</x-slot:title>
+    <h3>Welcome to Contact Page</h3>
+</x-layout>
