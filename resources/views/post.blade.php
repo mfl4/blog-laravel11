c53b2a6552c9dd9e2f813e00cbd652ae44964978
mfl4 <mhdfarhanl6267@gmail.com>
2024-06-30 08:09:46 +0700

    feat(resources): added a new view to display singular post

---
+++ resources/views/post.blade.php
@@ -0,0 +1,13 @@
+<x-layout>
+    <x-slot:title>{{ $title }}</x-slot:title>
+
+    <article class="max-w-screen-md py-8">
+        <h2 class="mb-1 text-3xl font-bold tracking-tight text-gray-900">{{ $post['title'] }}</h2>
+        <div class="text-base text-gray-500">
+            <a href="#">{{ $post['author'] }}</a> | {{ Str::random(10) }}
+        </div>
+        <p class="my-4 font-light">{{ $post['body'] }}</p>
+        <a href="/posts" class="font-medium text-blue-500 hover:underline">&laquo; Back to Posts</a>
+    </article>
+
+</x-layout>
