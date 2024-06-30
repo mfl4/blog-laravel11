3cf61283f53b7c402d211b30a46a94fedd79781e
mfl4 <mhdfarhanl6267@gmail.com>
2024-06-30 10:37:16 +0700

    fix(resources): create dynamic dates according to the database

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -4,7 +4,7 @@
     <article class="max-w-screen-md py-8">
         <h2 class="mb-1 text-3xl font-bold tracking-tight text-gray-900">{{ $post['title'] }}</h2>
         <div class="text-base text-gray-500">
-            <a href="#">{{ $post['author'] }}</a> | {{ Str::random(10) }}
+            <a href="#">{{ $post['author'] }}</a> | {{ $post['created_at']->diffForHumans() }}
         </div>
         <p class="my-4 font-light">{{ $post['body'] }}</p>
         <a href="/posts" class="font-medium text-blue-500 hover:underline">&laquo; Back to Posts</a>
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -7,7 +7,7 @@
                 <h2 class="mb-1 text-3xl font-bold tracking-tight text-gray-900">{{ $post['title'] }}</h2>
             </a>
             <div class="text-base text-gray-500">
-                <a href="#">{{ $post['author'] }}</a> | {{ Str::random(10) }}
+                <a href="#">{{ $post['author'] }}</a> | {{ $post['created_at']->diffForHumans() }}
             </div>
             <p class="my-4 font-light">{{ Str::limit($post['body'], 150, '...') }}</p>
             <a href="/posts/{{ $post['slug'] }}" class="font-medium text-blue-500 hover:underline">Read more &raquo;</a>
