237bd4bb40390d6986eb65e3213ac426a9ca0c4b
mfl4 <mhdfarhanl6267@gmail.com>
2024-06-30 08:07:51 +0700

    refac(resources): renamed the blog view to posts to improve route conformance & readability

--- resources/views/blog.blade.php
+++
@@ -1,4 +0,0 @@
-<x-layout>
-    <x-slot:title>{{ $title }}</x-slot:title>
-    <h3>Welcome to Blog</h3>
-</x-layout>
---
+++ resources/views/posts.blade.php
@@ -0,0 +1,16 @@
+<x-layout>
+    <x-slot:title>{{ $title }}</x-slot:title>
+
+    @foreach ($posts as $post)
+        <article class="max-w-screen-md py-8 border-b border-gray-300">
+            <a href="/posts/{{ $post['slug'] }}" class="hover:underline">
+                <h2 class="mb-1 text-3xl font-bold tracking-tight text-gray-900">{{ $post['title'] }}</h2>
+            </a>
+            <div class="text-base text-gray-500">
+                <a href="#">{{ $post['author'] }}</a> | {{ Str::random(10) }}
+            </div>
+            <p class="my-4 font-light">{{ Str::limit($post['body'], 150, '...') }}</p>
+            <a href="/posts/{{ $post['slug'] }}" class="font-medium text-blue-500 hover:underline">Read more &raquo;</a>
+        </article>
+    @endforeach
+</x-layout>
